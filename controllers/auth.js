c8134c64a0fa9f54d665a22b5527c37ebb81035e
anshul45 <80778090+anshul45@users.noreply.github.com>
2023-02-11 21:39:45 +0530

    authenticated middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,16 +10,20 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  if (!email || password) {
+  if (!email || !password) {
     throw new BadRequestError("Please provide email and password");
   }
   const user = await User.findOne({ email });
-  //compare password
   if (!user) {
     throw new UnauthenticatedError("invalid Credition");
   }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("invalid Credition");
+  }
+  //compare password
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: { name: user.name } }, token);
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = { register, login };
--- models/User.js
+++ models/User.js
@@ -37,4 +37,9 @@ UserSchema.methods.createJWT = function () {
   );
 };
 
+UserSchema.methods.comparePassword = async function (password) {
+  const isMatch = await bcrypt.compare(password, this.password);
+  return isMatch;
+};
+
 module.exports = mongoose.model("User", UserSchema);
